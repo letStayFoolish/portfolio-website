71f454ea4e37b1aa396804c2fd7d76a9342bb0b2
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-30 13:25:53 +0700

    Typing Effect JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Add custom JavaScript here
+
+function typeText() {
+  const text = 'I am Nemanja Karaklajic.'
+  const textDOMElement = document.getElementById('typing-text')
+
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      textDOMElement.textContent += text[i]
+    }, 100 * i)
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', typeText)
